20fb976081ca416c8489ca829b621a3ffb05b331
Adnan Khan <adnan7.zain@gmail.com>
2022-01-08 22:45:39 +0500

    basic express app

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+// async error
+
+const express = require('express');
+const app = express();
+
+const errorMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1> <a href="/api/v1/products">products route</a>');
+});
+
+// product route
+
+app.use(errorMiddleware);
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //   connect db
+    app.listen(port, console.log(`Server listening on Port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
