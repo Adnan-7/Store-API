68e392361ae4ff3f6f3b3d7540956a138bf9e6b3
Adnan Khan <adnan7.zain@gmail.com>
2022-01-25 16:11:38 +0500

    set up routes for the products

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const errorMiddleware = require('./middleware/error-handler');
 const notFoundMiddleware = require('./middleware/not-found');
@@ -18,6 +20,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1> <a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter);
 // product route
 
 app.use(errorMiddleware);
