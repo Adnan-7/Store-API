801edd0bb6f2d89fb964d246b9e8a5b3b3fd33c3
Adnan Khan <adnan7.zain@gmail.com>
2022-01-26 21:52:52 +0500

    query params

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const product = await Product.find();
-  res.status(200).json({ product });
+  const products = await Product.find();
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
